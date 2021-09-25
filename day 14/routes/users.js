eee6fa20f7b02f816b672922523eb8b8c5884aba
agarwalnupur <54990479+agarwalnupur@users.noreply.github.com>
2021-09-25 18:31:43 +0530

    Add files via upload

---
+++ day 14/routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+
+//postgreSQL without sequelize
+
+const Pool = require('pg').Pool;
+
+const pool = new Pool ({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '1234567890',
+  port: 5432
+})
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  pool.query('SELECT * FROM "Users"', (err, result) => {
+    if(err) {
+      throw err;
+    }
+    res.status(200).json(result);
+  })
+  res.send('respond with a resource');
+});
+
+module.exports = router;
