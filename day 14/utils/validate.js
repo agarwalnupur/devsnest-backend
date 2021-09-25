a8d7bb05adbc58096b425a0e31efd98ecaf127bb
agarwalnupur <54990479+agarwalnupur@users.noreply.github.com>
2021-09-25 18:32:46 +0530

    Add files via upload

---
+++ day 14/utils/validate.js
@@ -0,0 +1,11 @@
+const emailValidate = (email) => {
+    const re = /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/ ;
+    return re.test(email);
+}
+
+const passwordValidate = (password) => {
+    const re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(password);
+}
+
+module.exports = {emailValidate, passwordValidate }
