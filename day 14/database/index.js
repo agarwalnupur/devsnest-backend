18093ab027529e51dcbc08fc575bf7c67d4b69f2
agarwalnupur <54990479+agarwalnupur@users.noreply.github.com>
2021-09-25 18:26:06 +0530

    Create index.js

---
+++ day 14/database/index.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require("sequelize");
+const {
+  sequelize_database,
+  sequelize_username,
+  sequelize_password,
+  sequelize_host,
+  sequelize_dialect,
+} = require("../config/index");
+const sequelize = new Sequelize(
+  sequelize_database,
+  sequelize_username,
+  sequelize_password,
+  {
+    host: sequelize_host,
+    dialect: sequelize_dialect,
+  }
+);
+
+sequelize.sync();
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection established with database");
+  } catch (err) {
+    console.error("Unable to connect to db");
+  }
+})();
+
+module.exports = sequelize;
