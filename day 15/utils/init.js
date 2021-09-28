288e8f168d8c0b8d70bcc28dec575dfea68e80bb
agarwalnupur <54990479+agarwalnupur@users.noreply.github.com>
2021-09-28 14:11:56 +0530

    Add files via upload

---
+++ day 15/utils/init.js
@@ -0,0 +1,23 @@
+const sequelize = require("../database");
+const product = require("../models/products");
+
+sequelize
+  .sync()
+  .then((...a) => {
+    const now = Date.now();
+    for (let i = 0; i < 100; i++) {
+      const productId = now + i;
+      await product.create({
+        title: `product title ${productId}`,
+        price: Math.floor(100 + Math.random() * 1000),
+        count: Math.floor(1 + Math.random() * 1000),
+        description: `product description ${productId}`,
+        image: `https://picsum.photos/100?product=${productId}`,
+      });
+    }
+  })
+  .catch(async (e) => {
+    await sequelize.close();
+    console.log(e);
+  })
+  .catch((e) => console.log);
